d82c94c25390c5e6958e5db65088e2ddba1eebce
Sergey Kozlov <serega-kozlov@mail.ru>
2024-09-15 22:32:44 +0300

    radiobutton for main window

--- x1fd3/gui/main_window.py
+++ x1fd3/gui/main_window.py
@@ -14,100 +14,78 @@ class MainWindow:
         draw buttons for mode selection
         '''
         self.root = tk.Tk()
-        self.mode = tk.StringVar()
 
         self.root.title('x1fd3')
         self.root.resizable(False, False)
 
+        self.mode = tk.StringVar()
+
         ttk.Label(
-            text = 'Select runtype mode',
+            text = 'Mode',
             font = ('bold', 16)
         ).grid(
             row = 0,
-            column = 0
-            #columnspan = 4
+            column = 0,
+            sticky = 'w'
+        )
+
+        ttk.Label(
+            text = 'Description',
+            font = ('bold', 16)
+        ).grid(
+            row = 0,
+            column = 1,
+            sticky = 'w'
         )
 
         ttk.Separator(
             orient = 'horizontal'
         ).grid(
             row = 1,
-            column = 0
-            #columnspan = 4,
-            #sticky = 'ew'
+            column = 0,
+            columnspan = 2,
+            sticky = 'ew'
         )
 
-        ttk.Button(
-            text = 'Point-wise PEC approximation with analytic function (EMO)',
-            #value = 'PecApprox',
-            #variable = self.mode,
-            command = lambda: self.select_mode('PecApprox')
-        ).grid(
-            row = 2,
-            column = 0
-            #sticky = 'w'
-        )
+        modes = {
+            'PecApprox': 'Point-wise PEC approximation with analytic function (EMO)',
+            'LevelsPW': 'Vib.-rot. levels calculation with point-wise PEC',
+            'LevelsAn': 'Vib.-rot. levels calculation with analytic PEC (EMO)',
+            'SpectrumPW': 'Vib.-rot. spectrum calculation with point-wise PEC',
+            'SpectrumAn': 'Vib.-rot. spectrum calculation with analytic PEC (EMO)',
+            'FitExp': 'Fit PEC to reproduce experimental vib.-rot. levels'
+        }
 
-        ttk.Button(
-            text = 'Vib.-rot. levels calculation with point-wise PEC',
-            #value = 'LevelsPW',
-            #variable = self.mode,
-            command = lambda: self.select_mode('LevelsPW')
-        ).grid(
-            row = 3,
-            column = 0
-            #sticky = 'w'
-        )
+        ttk.Style().configure('my.TRadiobutton', font = ('bold'), foreground = 'red')
 
-        ttk.Button(
-            text = 'Vib.-rot. levels calculation with analytic PEC (EMO)',
-            #value = 'LevelsAn',
-            #variable = self.mode,
-            command = lambda: self.select_mode('LevelsAn')
-        ).grid(
-            row = 4,
-            column = 0
-            #sticky = 'w'
-        )
+        row = 2
+        for mode, descr in modes.items():
+            ttk.Radiobutton(
+                text = mode,
+                style = 'my.TRadiobutton',
+                value = mode,
+                variable = self.mode,
+                command = self.select_mode
+            ).grid(
+                row = row,
+                column = 0,
+                sticky = 'w'
+            )
 
-        ttk.Button(
-            text = 'Vib.-rot. spectrum calculation with point-wise PEC',
-            #value = 'SpectrumPW',
-            #variable = self.mode,
-            command = lambda: self.select_mode('SpectrumPW')
-        ).grid(
-            row = 5,
-            column = 0
-            #sticky = 'w'
-        )
-
-        ttk.Button(
-            text = 'Vib.-rot. spectrum calculation with analytic PEC (EMO)',
-            #value = 'SpectrumAn',
-            #variable = self.mode,
-            command = lambda: self.select_mode('SpectrumAn')
-        ).grid(
-            row = 6,
-            column = 0
-            #sticky = 'w'
-        )
-
-        ttk.Button(
-            text = 'Fit PEC to reproduce experimental vib.-rot. levels',
-            #value = 'FitExp',
-            #variable = self.mode,
-            command = lambda: self.select_mode('FitExp')
-        ).grid(
-            row = 7,
-            column = 0
-            #sticky = 'w'
-        )
+            tk.Label(
+                text = descr
+            ).grid(
+                row = row,
+                column = 1,
+                sticky = 'w'
+            )
+            
+            row += 1
 
     def select_mode(
-        self,
-        mode: str
+        self
     ) -> None:
         '''
         draw sub window for calculation
         '''
-        CalcWindow(self.root, mode)
+        CalcWindow(self.root, self.mode.get())
