d3b5e996512827fe56ed98b6cf57ece03f7a5064
Sergey Kozlov <serega-kozlov@mail.ru>
2025-06-24 19:46:35 +0300

    fix 5

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup # type:ignore
+
+setup(
+    name='x1fd3',
+    packages=find_packages(),
+    version='0.0.1',
+    description='a program to deal with the adiabatic state of diatomic molecules.',
+    author='Sergey Kozlov',
+    license='BSD 3-Clause',
+)
